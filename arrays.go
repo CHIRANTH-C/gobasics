d6a57e50d7969d14365a8ce17cf7a5250dcad380
CHIRANTH <chiranthchandrashekar123@gmail.com>
2020-04-28 08:01:58 +0530

    adding descriptions

--- arrays.go
+++ arrays.go
@@ -5,9 +5,17 @@ import "fmt"
 func ArrayForLoop() {
 	var names [6]string
 	names = [6]string{"Jim", "Frank", "Troy", "Marry", "Jenny", "Anny"}
+
+	fmt.Println("*******************")
+	fmt.Println("Pass By Value")
+	fmt.Println("*******************")
+
 	for i, v := range names {
 		fmt.Println(i, v)
 	}
+	fmt.Println("*******************")
+	fmt.Println("Pass By Referance")
+	fmt.Println("*******************")
 	for i := range names {
 		fmt.Println(names[i])
 	}
