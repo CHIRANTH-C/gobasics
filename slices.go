adfa538a670672d6b5f0e9cd89e3efcb550e0d34
CHIRANTH <chiranthchandrashekar123@gmail.com>
2020-05-02 11:45:23 +0530

    correcting fmt statement

--- slices.go
+++ slices.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func Slicespro() {
 	studentnames := []string{"Chiranth", "Hemanth", "Rakesh"}
-	fmt.Println{"*******************"}
+	fmt.Println("*******************")
 	for index, value := range studentnames {
 		fmt.Printf("Rol No : %v - Name : %v", index, value)
 	}
